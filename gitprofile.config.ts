d84f8843e47d7f02d6a3219c49bf0b82baf1b553
Mohammad Salhi <49008428+out-of-context@users.noreply.github.com>
2024-08-01 19:30:23 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'updated', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['out-of-context/tnt-dj', ''out-of-context/tnt-dj-be', 'out-of-context/alone-with-a-stranger','out-of-context/portfolio'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
@@ -69,7 +69,7 @@ const CONFIG = {
     dribbble: '',
     behance: '',
     medium: '',
-    dev: '',
+    dev: 'https://leetcode.com/u/out-of-context/',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
